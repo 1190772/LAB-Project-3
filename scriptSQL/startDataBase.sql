5ef2b687fd0347b1a6f5ce383f50df6adea275ef
DESKTOP-V9SBN6U\Samuel <1201274@isep.ipp.pt>
2021-11-08 21:46:48 +0000

    Continuação do desenvolvimento da US108, falta apenas alguns detalhes em termos de tamanhos de variáveis e teste do script para a base de dados (problemas técnicos na tentativa de acesso à base de dados fora do ISEP)

---
+++ scriptSQL/startDataBase.sql
@@ -0,0 +1,137 @@
+drop table Container
+drop table Cargo_Manifesto
+drop table Truck
+drop table Ship
+drop table Port
+drop table Warehouse
+drop table Employee
+drop table Role
+drop table Container_Length
+drop table Width_Height
+drop table Container_Type
+drop table Reduced_Strength
+drop table ISO
+drop table ID_Container
+drop table Owner
+drop table Equipment_Identifier
+drop table Position_Ship
+
+create table Container(
+    id_container              char(11) constraint pk_container Primary Key,
+    tare                      integer,
+    iso_code                  char(4),
+    refrigeration_temperature number(3,1),
+    max_weight_incl_container integer,
+    max_weight                integer,
+    max_volume                integer,
+--    csc_plate                 char(?),
+    acep                      integer,
+    pes_date                  date);
+
+create table Owner(
+    id_owner                  char(3) constraint pk_owner Primary Key,
+    name                      varChar(30));
+
+create table Equipment_Identifier(
+    id_equipment              char(1) constraint pk_equipment_identifier Primary Key,
+    description               varChar(100));
+
+create table ID_Container(
+    id_container              char(11) constraint pk_id_container Primary Key,
+    id_owner                  char(3) constraint fk_owner_id_container Foreign Key (id_owner) references Owner(id_owner),
+    id_equipment              char(1) constraint fk_equipment_id_container Foreign Key (id_equipment) references Equipment_Identifier(id_equipment),
+    serial_number             integer(6) constraint un_serial_number_id_container unique,
+    check_digit               integer(1));
+
+create table Container_Length(
+    length_code               char(1) constraint pk_container_length Primary Key,
+    value_length              integer);
+
+create table Width_Height(
+    width_height_code         char(1) constraint pk_width_height Primary Key,
+    value_height              integer);
+    value_width               integer);
+
+create table Container_Type(
+    container_type_code       char(1) constraint pk_container_type Primary Key,
+    value_container_type      integer);
+
+create table Reduce_Strength(
+    reduced_strength_code     char(1) constraint pk_reduced_strength Primary Key,
+    value_reduced_strength    integer);
+
+create table ISO(
+    iso_code                  char(4) constraint pk_iso Primary Key,
+    length_code               char(1) constraint fk_length_iso Foreign Key (length_code) references Container_Length(length_code),
+    width_height_code         char(1) constraint fk_width_height_iso Foreign Key (width_height_code) references Width_Height(width_height_code),
+    container_type_code       char(1) constraint fk_container_type_iso Foreign Key container_type_code references Container_Type(container_type_code),
+    reduced_strength_code     char(1) constraint fk_reduced_strength_iso Foreign Key (reduced_strength_code) references Reduced_Strength(reduced_strength_code);
+
+create table Role(
+    role_id       			  integer(2) constraint pk_role Primary Key,
+    designation			      varChar(30));
+
+create table Employee(
+--    id_employee		    	  char(?) contraint pk_employee Primary Key,
+    role_id		    	      integer(2) constraint fk_role_id_employee Foreign Key (role_id) references Role(role_id),
+    name_employee			  varChar(30),
+    user_password			  varChar(15),
+    address_employee		  varChar(30),
+    phone_employee			  integer(20));
+
+create table Truck(
+--    id_truck                  char(?) constraint pk_truck Primary Key,
+--    id_employee               char(?) constraint fk_id_employee_truck references Employee(id_employee)
+);
+
+create table Ship(
+    imo_code                  char(10) constraint pk_ship Primary Key,
+    mmsi_code                 integer(9) constraint un_mmsi_code_ship unique, -- Primary Key,
+    name_ship                 varChar(30),
+--    number_generators         integer(2)?,
+--    power_out_generator       integer(3/4)?,
+    call_sign                 char(5) constraint un_call_sign_ship unique,
+    vessel_type               integer(2),
+    Length_ship               integer(3),
+    width_ship                integer(2),
+    draft                     number(3,1),
+    capacity_ship             integer(5)) null;
+
+create table Position_Ship(
+--    id_ship                   char(?) constraint fk_ship_position_ship Foreign Key (id_ship) references Ship(?),
+    base_date_time  	      date,
+    latitude			      number(7,5),
+    longitude			      number(7,5),
+    sog	    			      number(3,1),
+    cog 			          number(3,1),
+    heading			          integer(3),
+    transceiver_class         char(1)
+-- Constraint pk_position_ship Primary Key (id_ship, base_date_time)
+    );
+
+create table Port(
+    id_port			          char(5) constraint pk_port Primary Key,
+    name				      varChar(20),
+    continent			      varChar(10),
+    country			          varChar(20),
+    latitude			      number(11,9),
+    longitude			      number(11,9));
+
+create table Warehouse(
+--    id_warehouse     		  char(?) constraint pk_warehouse Primary Key,
+    name    	   			  varChar(30),
+    continent		    	  varChar(20),
+    country		         	  varChar(20),
+    latitude		    	  number(4,2),
+    longitude		    	  number(4,2));
+
+create table Cargo_Manifesto(
+    id_container              char(11) constraint fk_id_container_cargo_manifesto Foreign Key (id_container) references Container(id_container),
+    date_time                 date,
+--    id_truck                  char(?) constraint fk_truck_cargo_manifesto Foreign Key (id_truck) references Truck(id_truck),
+--    id_ship                   char(?) constraint fk_ship_cargo_manifesto Foreign Key (id_ship) references Ship(?),
+    id_port                   char(5) constraint fk_id_port_cargo_manifesto Foreign Key (id_port) references Port(id_port),
+--    id_warehouse              char(?) constraint fk_id_warehouse_cargo_manifesto Foreign Key (id_warehouse) references Warehouse(id_warehouse),
+    position_code             integer(6),
+    payload               number(5,1)
+    Constraint pk_cargo_manifesto Primary Key Cargo_Manifesto(id_container, date_time));
