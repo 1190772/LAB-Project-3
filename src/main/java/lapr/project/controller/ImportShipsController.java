782d9f59b6d3b3c5765917265398dc232bd6c5d9
José Silva <1190772@isep.ipp.pt>
2021-11-11 18:13:42 +0000

    Alterações

--- src/main/java/lapr/project/controller/ImportShipsController.java
+++ src/main/java/lapr/project/controller/ImportShipsController.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class ImportShipsController {
 
@@ -49,95 +50,47 @@ public class ImportShipsController {
     //TODO
 
 
+
     public void importShips() {
         String[] parameters;
         try {
-            in = new BufferedReader((new FileReader(fileName)));
-            parameters = in.readLine().split(",");
-            mmsi = parameters[0];
-            dateTime = parameters[1];
-            year = Integer.parseInt(dateTime.substring(6, 10));
-            month = Integer.parseInt(dateTime.substring(3, 5));
-            day = Integer.parseInt(dateTime.substring(0, 2));
-            hour = Integer.parseInt(dateTime.substring(11, 13));
-            minute = Integer.parseInt(dateTime.substring(14, 16));
-            latitude = Double.parseDouble(parameters[2]);
-            longitude = Double.parseDouble(parameters[3]);
-            sog = Double.parseDouble(parameters[4]);
-            cog = Double.parseDouble(parameters[5]);
-            heading = Double.parseDouble(parameters[6]);
-            name = parameters[7];
-            imo = parameters[8];
-            callSign = parameters[9];
-            vesselType = Integer.parseInt(parameters[10]);
-            length = Integer.parseInt(parameters[11]);
-            width = Integer.parseInt(parameters[12]);
-            draft = Float.parseFloat(parameters[13]);
-            cargo = parameters[14];
-            transceiverClass = parameters[15].charAt(0);
+            Scanner in = new Scanner((new FileReader(fileName)));
+            while (in.hasNextLine()) {
+                parameters = in.nextLine().split(",");
+                mmsi = parameters[0];
+                dateTime = parameters[1];
+                year = Integer.parseInt(dateTime.substring(6, 10));
+                month = Integer.parseInt(dateTime.substring(3, 5));
+                day = Integer.parseInt(dateTime.substring(0, 2));
+                hour = Integer.parseInt(dateTime.substring(11, 13));
+                minute = Integer.parseInt(dateTime.substring(14, 16));
+                latitude = Double.parseDouble(parameters[2]);
+                longitude = Double.parseDouble(parameters[3]);
+                sog = Double.parseDouble(parameters[4]);
+                cog = Double.parseDouble(parameters[5]);
+                heading = Double.parseDouble(parameters[6]);
+                name = parameters[7];
+                imo = parameters[8];
+                callSign = parameters[9];
+                vesselType = Integer.parseInt(parameters[10]);
+                length = Integer.parseInt(parameters[11]);
+                width = Integer.parseInt(parameters[12]);
+                draft = Float.parseFloat(parameters[13]);
+                cargo = parameters[14];
+                transceiverClass = parameters[15].charAt(0);
 
-            LocalDateTime localDateTime = LocalDateTime.of(year, month, day, hour, minute);
+                LocalDateTime localDateTime = LocalDateTime.of(year, month, day, hour, minute);
 
-            Ship ship = new Ship(mmsi, name, imo, callSign, vesselType, length, width, draft, cargo);
-            ShipPosition shipPosition = new ShipPosition(year, month, day, hour, minute, latitude, longitude, sog, cog, heading, transceiverClass);
-            while ((word = in.readLine()) != null) {
-                bst.insert(ship);
+                Ship ship = new Ship(mmsi, name, imo, callSign, vesselType, length, width, draft, cargo);
+                ShipPosition shipPosition = new ShipPosition(year, month, day, hour, minute, latitude, longitude, sog, cog, heading, transceiverClass);
+                    bst.insert(ship);
+
+                System.out.println(ship);
+                System.out.println(shipPosition);
+                System.out.println();
             }
-            System.out.println(ship);
-            System.out.println(shipPosition);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-//
-//    public void importShipsByMMSI() {
-//        ship_BST_mmsi = new BST<>();
-//        try {
-//            in = new BufferedReader(new FileReader(fileName));
-//            String[] aux_array;
-//            in.readLine();
-//            while ((word = in.readLine()) != null) {
-//                aux_array = word.split(",");
-//                ship_BST_mmsi.insert(Integer.parseInt(aux_array[0]));
-//            }
-//            System.out.println(ship_BST_mmsi);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public void importShipsByIMO() {
-//        ship_BST_imo = new BST<>();
-//        try {
-//            in = new BufferedReader(new FileReader(fileName));
-//            String[] aux_array;
-//            in.readLine();
-//            while ((word = in.readLine()) != null) {
-//                aux_array = word.split(",");
-//                ship_BST_imo.insert((aux_array[8]));
-//            }
-//            System.out.println(ship_BST_imo);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public void importShipsByCallSign() {
-//        ship_BST_callSign = new BST<>();
-//        try {
-//            in = new BufferedReader(new FileReader(fileName));
-//            String[] aux_array;
-//            in.readLine();
-//            while ((word = in.readLine()) != null) {
-//                aux_array = word.split(",");
-//                ship_BST_callSign.insert((aux_array[9]));
-//            }
-//            System.out.println(ship_BST_callSign);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
