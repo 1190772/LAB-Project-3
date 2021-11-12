6d56f8bd10aaa65e54b7e76c01a70d58a6f2aad3
1190772 <1190772@isep.ipp.pt>
2021-11-12 02:32:48 +0000

    UC106 Beginning

---
+++ src/main/java/lapr/project/controller/TopNShipsController.java
@@ -0,0 +1,17 @@
+package lapr.project.controller;
+
+import lapr.project.model.*;
+
+import java.util.Scanner;
+
+public class TopNShipsController {
+
+    private ShipBST shipBST;
+    private ShipMovements shipMovements;
+
+    public void topNShips(){
+        Scanner in = new Scanner(System.in);
+        in.nextLine();
+    }
+
+}
