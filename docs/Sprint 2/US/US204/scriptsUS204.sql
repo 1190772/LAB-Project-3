076d1b0f881cc969c7bb32b0b273bdc317c4988c
1190772 <1190772@isep.ipp.pt>
2021-12-05 22:24:59 +0000

    US 204 - Review e correções

--- docs/Sprint 2/US/US204/scriptsUS204.sql
+++ docs/Sprint 2/US/US204/scriptsUS204.sql
@@ -1,72 +1,73 @@
-CREATE OR REPLACE FUNCTION Container_situation (id container.id_container%type) RETURN varchar
+CREATE OR REPLACE FUNCTION Container_situation(id container.id_container%type) RETURN varchar 
 is 
-max_date date; 
-manifest_type number;
-id_cargo_manifest integer;
-id_trip integer;
-id_ship char(10);
-id_port char(5);
-name_ship varchar(30);
-name_port varchar(20);
+max_date1 TIMESTAMP; 
+manifest_type1 integer; 
+id_cargo_manifest1 integer;
+id_trip1 integer;
+id_ship1 char(10);
+id_port1 char(5);
+name_ship1 varchar(30);
+name_port1 varchar(20);
 NO_DATA_FOUND exception;
 
-BEGIN
-
-SELECT max(t.date_time_start) INTO max_date
+BEGIN 
+ 
+SELECT max(t.date_time_start) INTO max_date1 
 FROM cargo_manifest cm , trip t
 WHERE cm.id_container=id and cm.id_trip=t.id_trip;
 
-IF max_date = null
+IF max_date1 = null
     THEN
         RAISE NO_DATA_FOUND;
 END IF;
 
-SELECT cm.id_cargo_manifest INTO id_cargo_manifest
+SELECT cm.id_cargo_manifest INTO id_cargo_manifest1
 FROM cargo_manifest cm, trip t
-WHERE cm.id_trip=t.id_trip and t.date_time_start=max_date;
+WHERE cm.id_trip=t.id_trip and t.date_time_start=max_date1;
 
-SELECT count() INTO manifest_type
+SELECT count(id_cargo_manifest1) INTO manifest_type1
 FROM unloading_cargo_manifest uc
-WHERE id_cargo_manifest=uc.id_cargo_manifest and id=uc.id_container;
+WHERE id_cargo_manifest1=uc.id_cargo_manifest and id=uc.id_container;
 
 
-
-IF manifest_type = 0
-    THEN
-        SELECT t.ship_imo INTO id_ship
+IF manifest_type1 = 0
+    THEN 
+       
+        SELECT t.ship_imo INTO id_ship1
         FROM cargo_manifest cm, trip t
-        WHERE cm.id_trip=t.id_trip and t.date_time_start=max_date;
+        WHERE cm.id_trip=t.id_trip and t.date_time_start=max_date1;
 
-        SELECT ship.name_ship INTO name_ship
+        SELECT ship.name_ship INTO name_ship1
         FROM ship
-        WHERE ship.imo_code=id_ship;
-
-        RETURN name_ship;
-
-    ELSE
-        SELECT t.id_destination_port INTO id_port
+        WHERE ship.imo_code=id_ship1;
+        
+        RETURN CONCAT ('Ship: ' ,name_ship1);
+        
+    ELSE 
+        
+        SELECT t.id_destination_port INTO id_port1
         FROM cargo_manifest cm, trip t
-        WHERE cm.id_trip=t.id_trip and t.date_time_start=max_date;
-
-        SELECT port.name INTO name_port
-        FROM port
-        WHERE port.id_port = id_port;
-
-        RETURN name_port;
-
-    END IF;
-
-
-    EXCEPTION
+        WHERE cm.id_trip=t.id_trip and t.date_time_start=max_date1;
+    
+        SELECT name INTO name_port1
+        FROM port p
+        WHERE p.id_port = id_port1;
+        
+        RETURN CONCAT ('Port: ' ,name_port1);
+        
+    END IF; 
+    
+    
+    EXCEPTION 
     WHEN NO_DATA_FOUND THEN 
  
-        DBMS_OUTPUT.PUT_LINE('No data found');
-        RETURN null;
-
-END;
+        DBMS_OUTPUT.PUT_LINE('No data found'); 
+        RETURN null; 
+        
+END; 
 /
 
-SELECT Container_situation('BICU1234561') as "Result"
+SELECT Container_situation('BICU1234561') as "Container´s current situation:"
 from dual;
 
 
