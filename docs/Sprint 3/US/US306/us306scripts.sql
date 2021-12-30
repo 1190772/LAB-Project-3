f016a16957655b2c7617bd6fc6501b3de791cc68
1190772 <1190772@isep.ipp.pt>
2021-12-30 17:15:07 +0000

    Mais correçoes US306

--- docs/Sprint 3/US/US306/us306scripts.sql
+++ docs/Sprint 3/US/US306/us306scripts.sql
@@ -23,7 +23,7 @@ BEGIN
     WHERE trip.id_destination_warehouse = id_warehouse AND trip.date_time_end>minDate and trip.date_time_end<maxDate;
     
     SELECT count(*) into containersOutB  
-        FROM CARGO_MANIFEST JOIN TRIP ON cargo_manifest.id_cargo_manifest = trip.id_trip RIGHT JOIN LOADING_CARGO_MANIFEST on cargo_manifest.id_cargo_manifest = loading_cargo_manifest.id_cargo_manifest
+        FROM CARGO_MANIFEST JOIN TRIP ON cargo_manifest.id_cargo_manifest = trip.id_trip RIGHT JOIN UNLOADING_CARGO_MANIFEST on cargo_manifest.id_cargo_manifest = unloading_cargo_manifest.id_cargo_manifest
     WHERE trip.id_destination_warehouse = id_warehouse AND trip.date_time_end>minDate and trip.date_time_end<maxDate;
     
     fill := containersInB - containersOutB + containersIn - containersOut;
