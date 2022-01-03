63132dbb831c1770b8fa3d0373f7e7c739657eac
1190772 <1190772@isep.ipp.pt>
2022-01-03 22:46:25 +0000

    US 306/US 307 alterações

--- docs/Sprint 3/US/US307/us307scriptsV2.sql
+++ docs/Sprint 3/US/US307/us307scriptsV2.sql
@@ -1,28 +1,28 @@
 CREATE OR REPLACE TRIGGER Accomodate_Manifest BEFORE INSERT OR UPDATE ON UNLOADING_CARGO_MANIFEST
-                                                                   for each row
+for each row
 
 declare
-warehouse_capacity integer;
-number_containers integer;
+viewOccupancyRateV number;
+fill integer;
 
 BEGIN
 
-SELECT capacity INTO warehouse_capacity
-FROM warehouse h
-WHERE h.id_warehouse = :new.id_warehouse;
+ViewOccupancyRate(:new.id_warehouse, viewOccupancyRateV, fill);
 
-SELECT count(*) INTO number_containers
-FROM UNLOADING_CARGO_MANIFEST
+IF viewOccupancyRateV = 100 THEN
+DELETE FROM unloading_cargo_manifest
 WHERE id_cargo_manifest = :new.id_cargo_manifest;
-
-IF number_containers > warehouse_capacity THEN
+DELETE FROM loading_cargo_manifest
+WHERE id_cargo_manifest = :new.id_cargo_manifest;
+DELETE FROM cargo_manifest
+WHERE id_cargo_manifest = :new.id_cargo_manifest;
 raise_application_error(-20111, 'You have reached the warehouse capacity');
 end if;
-
+    
 END Accomodate_Manifest;
 /
 
-INSERT INTO WAREHOUSE VALUES(11225, 'Sitio', 'PT', 40, 40, 2 , 'PT345');
+INSERT INTO WAREHOUSE VALUES(11225, 'Sitio', 'PT', 40, 40, 2 , 'PT345'); 
 
 
 INSERT INTO Cargo_Manifest VALUES (98765,'BICU1234561',1231, 120703, 23,  7, 8884441);
@@ -34,5 +34,3 @@ INSERT INTO Loading_Cargo_Manifest VALUES (98765,'ADIJ6543223',11225);
 INSERT INTO Unloading_Cargo_Manifest VALUES (98765,'BICU1234561',11225);
 INSERT INTO Unloading_Cargo_Manifest VALUES (98765,'NIKR1352462',11225);
 INSERT INTO Unloading_Cargo_Manifest VALUES (98765,'ADIJ6543223',11225);
-    
-    
