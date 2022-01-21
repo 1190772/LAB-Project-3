d701eb532da6d3e3b359d031f9a2d0c686f4debd
DESKTOP-V9SBN6U\Samuel <1201274@isep.ipp.pt>
2022-01-21 22:29:18 +0000

    Remoção de code smells

--- src/main/java/lapr/project/data/DatabaseFunctions.java
+++ src/main/java/lapr/project/data/DatabaseFunctions.java
@@ -11,22 +11,23 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DatabaseFunctions {
-    private static final String shipIMO = "ship_imo char(10)";
-    private static final String idTruck = "id_truck integer";
-    private static final String idContainer = "id_container char(11)";
-    private static final String positionCode = "position_code number(6,0)";
-    private static final String cargoWeight = "cargo_weight integer";
-    private static final String refrigerationTemperature = "refrigeration_temperature number(3,1)";
-    private static final String tare = "tare integer";
-    private static final String valueLength = "value_length integer";
-    private static final String valueWidth = "value_width integer";
-    private static final String valueHeight = "value_height integer";
-    private static final String dateTimeStart = "date_time_start timestamp";
-    private static final String dateTimeEnd = "date_time_end timestamp";
-    private static final String idStartPort = "id_start_port char(6)";
-    private static final String idDestinationPort = "id_destination_port char(6)";
-    private static final String idStartWarehouse = "id_start_warehouse integer";
-    private static final String idDestinationWarehouse = "id_destination_warehouse integer";
+    private static final String SHIP_IMO = "ship_imo char(10)";
+    private static final String ID_TRUCK = "id_truck integer";
+    private static final String ID_CONTAINER = "id_container char(11)";
+    private static final String POSITION_CODE = "position_code number(6,0)";
+    private static final String CARGO_WEIGHT = "cargo_weight integer";
+    private static final String REFRIGERATION_TEMPERATURE = "refrigeration_temperature number(3,1)";
+    private static final String TARE = "tare integer";
+    private static final String VALUE_LENGTH = "value_length integer";
+    private static final String VALUE_WIDTH = "value_width integer";
+    private static final String VALUE_HEIGHT = "value_height integer";
+    private static final String DATE_TIME_START = "date_time_start timestamp";
+    private static final String DATE_TIME_END = "date_time_end timestamp";
+    private static final String ID_START_PORT = "id_start_port char(6)";
+    private static final String ID_DESTINATION_PORT = "id_destination_port char(6)";
+    private static final String ID_START_WAREHOUSE = "id_start_warehouse integer";
+    private static final String ID_DESTINATION_WAREHOUSE = "id_destination_warehouse integer";
+    private static final String DROP_TABLE_RESULT = "drop table result";
 
     public static void executeLine(DatabaseConnection databaseConnection, String sqlLine) {
         try {
@@ -68,22 +69,7 @@ public class DatabaseFunctions {
         }
     }
 
-    public static String getContainersRoute(DatabaseConnection databaseConnection, String clientID, String containerID) {
-        loadFunction(databaseConnection, "docs/Sprint 3/US/US305/US305Script.sql");
-
-        StringBuilder result = new StringBuilder();
-        result.append("Route:\nContainer: ").append(containerID).append("\n");
-        String function = "getContainersRoute(" + clientID + ", '" + containerID + "')";
-        String columnsForTable = shipIMO + ",\n" +
-                idTruck + ",\n" +
-                idStartPort + ",\n" +
-                idDestinationPort + ",\n" +
-                idStartWarehouse + ",\n" +
-                idDestinationWarehouse + ",\n" +
-                dateTimeStart + ",\n" +
-                dateTimeEnd;
-
-        Connection connection = databaseConnection.getConnection();
+    private static String prepForResultSet(DatabaseConnection databaseConnection, String function, String columnsForTable) {
         String sqlCommand = "create table result(" + columnsForTable + ")";
         executeLine(databaseConnection, sqlCommand);
 
@@ -101,10 +87,29 @@ public class DatabaseFunctions {
                 "        end;\n";
         executeLine(databaseConnection, sqlCommand);
 
-        sqlCommand = "select * from result";
+        return "select * from result";
+    }
+
+    public static String getContainersRoute(DatabaseConnection databaseConnection, String clientID, String containerID) {
+        loadFunction(databaseConnection, "docs/Sprint 3/US/US305/US305Script.sql");
+
+        StringBuilder result = new StringBuilder();
+        result.append("Route:\nContainer: ").append(containerID).append("\n");
+        String function = "getContainersRoute(" + clientID + ", '" + containerID + "')";
+        String columnsForTable = SHIP_IMO + ",\n" +
+                ID_TRUCK + ",\n" +
+                ID_START_PORT + ",\n" +
+                ID_DESTINATION_PORT + ",\n" +
+                ID_START_WAREHOUSE + ",\n" +
+                ID_DESTINATION_WAREHOUSE + ",\n" +
+                DATE_TIME_START + ",\n" +
+                DATE_TIME_END;
+
+        Connection connection = databaseConnection.getConnection();
 
 
-        try (PreparedStatement resultPreparedStatement = connection.prepareStatement(sqlCommand)) {
+
+        try (PreparedStatement resultPreparedStatement = connection.prepareStatement(prepForResultSet(databaseConnection, function, columnsForTable))) {
             try (ResultSet resultSet = resultPreparedStatement.executeQuery()) {
                 while (resultSet.next()) {
                     String aid = resultSet.getString(1);
@@ -134,8 +139,7 @@ public class DatabaseFunctions {
             databaseConnection.registerError(ex);
         }
 
-        sqlCommand = "drop table result";
-        executeLine(databaseConnection, sqlCommand);
+        executeLine(databaseConnection, DROP_TABLE_RESULT);
 
         return result.toString();
     }
@@ -146,33 +150,14 @@ public class DatabaseFunctions {
         StringBuilder result = new StringBuilder();
         result.append("Ship: ").append(shipIMO).append("\n");
         String function = "getContainersOfShip('" + shipIMO + "')";
-        String columnsForTable = idContainer + ",\n" +
-                positionCode + ",\n" +
-                cargoWeight + ",\n" +
-                refrigerationTemperature;
+        String columnsForTable = ID_CONTAINER + ",\n" +
+                POSITION_CODE + ",\n" +
+                CARGO_WEIGHT + ",\n" +
+                REFRIGERATION_TEMPERATURE;
 
         Connection connection = databaseConnection.getConnection();
-        String sqlCommand = "create table result(" + columnsForTable + ")";
-        executeLine(databaseConnection, sqlCommand);
 
-        sqlCommand = "declare\n" +
-                "        tempCursor SYS_REFCURSOR;\n" +
-                "        type resultRowType is table of result%rowtype;\n" +
-                "        c_t resultRowType;\n" +
-                "        begin\n" +
-                "        tempCursor:=" + function + ";\n" +
-                "        fetch tempCursor bulk collect into c_t;\n" +
-                "        close tempCursor;\n" +
-                "        forall indx in c_t.first..c_t.last\n" +
-                "        insert into result\n" +
-                "        values c_t(indx);\n" +
-                "        end;\n";
-        executeLine(databaseConnection, sqlCommand);
-
-        sqlCommand = "select * from result";
-
-
-        try (PreparedStatement resultPreparedStatement = connection.prepareStatement(sqlCommand)) {
+        try (PreparedStatement resultPreparedStatement = connection.prepareStatement(prepForResultSet(databaseConnection, function, columnsForTable))) {
             try (ResultSet resultSet = resultPreparedStatement.executeQuery()) {
                 while (resultSet.next()) {
                     result.append("id_container = ").append(resultSet.getString(1));
@@ -191,8 +176,7 @@ public class DatabaseFunctions {
             databaseConnection.registerError(ex);
         }
 
-        sqlCommand = "drop table result";
-        executeLine(databaseConnection, sqlCommand);
+        executeLine(databaseConnection, DROP_TABLE_RESULT);
 
         return result.toString();
     }
@@ -221,41 +205,22 @@ public class DatabaseFunctions {
         StringBuilder result = new StringBuilder();
         result.append("Date/Time/Ship/Truck/Container/Position/Refrigeration Temperature/Length/Width/Height/Total weight\n");
         String function = "getContainersToUnloadInVehicleNextWeek('" + portID + "')";
-        String columnsForTable = dateTimeEnd + ",\n" +
-                shipIMO + ",\n" +
-                idTruck + ",\n" +
-                idContainer + ",\n" +
-                positionCode + ",\n" +
-                cargoWeight + ",\n" +
-                refrigerationTemperature + ",\n" +
-                tare + ",\n" +
-                valueLength + ",\n" +
-                valueWidth + ",\n" +
-                valueHeight;
+        String columnsForTable = DATE_TIME_END + ",\n" +
+                SHIP_IMO + ",\n" +
+                ID_TRUCK + ",\n" +
+                ID_CONTAINER + ",\n" +
+                POSITION_CODE + ",\n" +
+                CARGO_WEIGHT + ",\n" +
+                REFRIGERATION_TEMPERATURE + ",\n" +
+                TARE + ",\n" +
+                VALUE_LENGTH + ",\n" +
+                VALUE_WIDTH + ",\n" +
+                VALUE_HEIGHT;
 
 
         Connection connection = databaseConnection.getConnection();
-        String sqlCommand = "create table result(" + columnsForTable + ")";
-        executeLine(databaseConnection, sqlCommand);
 
-        sqlCommand = "declare\n" +
-                "        tempCursor SYS_REFCURSOR;\n" +
-                "        type resultRowType is table of result%rowtype;\n" +
-                "        c_t resultRowType;\n" +
-                "        begin\n" +
-                "        tempCursor:=" + function + ";\n" +
-                "        fetch tempCursor bulk collect into c_t;\n" +
-                "        close tempCursor;\n" +
-                "        forall indx in c_t.first..c_t.last\n" +
-                "        insert into result\n" +
-                "        values c_t(indx);\n" +
-                "        end;\n";
-        executeLine(databaseConnection, sqlCommand);
-
-        sqlCommand = "select * from result";
-
-
-        try (PreparedStatement resultPreparedStatement = connection.prepareStatement(sqlCommand)) {
+        try (PreparedStatement resultPreparedStatement = connection.prepareStatement(prepForResultSet(databaseConnection, function, columnsForTable))) {
             try (ResultSet resultSet = resultPreparedStatement.executeQuery()) {
                 result.append("Containers to unload:\n\t");
                 while (resultSet.next()) {
@@ -274,46 +239,23 @@ public class DatabaseFunctions {
             databaseConnection.registerError(ex);
         }
 
-        sqlCommand = "drop table result";
-        executeLine(databaseConnection, sqlCommand);
+        executeLine(databaseConnection, DROP_TABLE_RESULT);
 
 
         function = "getContainersToLoadInVehicleNextWeek('" + portID + "')";
-        columnsForTable = dateTimeStart + ",\n" +
-                shipIMO + ",\n" +
-                idTruck + ",\n" +
-                idContainer + ",\n" +
-                positionCode + ",\n" +
-                cargoWeight + ",\n" +
-                refrigerationTemperature + ",\n" +
-                tare + ",\n" +
-                valueLength + ",\n" +
-                valueWidth + ",\n" +
-                valueHeight;
-        ;
+        columnsForTable = DATE_TIME_START + ",\n" +
+                SHIP_IMO + ",\n" +
+                ID_TRUCK + ",\n" +
+                ID_CONTAINER + ",\n" +
+                POSITION_CODE + ",\n" +
+                CARGO_WEIGHT + ",\n" +
+                REFRIGERATION_TEMPERATURE + ",\n" +
+                TARE + ",\n" +
+                VALUE_LENGTH + ",\n" +
+                VALUE_WIDTH + ",\n" +
+                VALUE_HEIGHT;
 
-
-        sqlCommand = "create table result(" + columnsForTable + ")";
-        executeLine(databaseConnection, sqlCommand);
-
-        sqlCommand = "declare\n" +
-                "        tempCursor SYS_REFCURSOR;\n" +
-                "        type resultRowType is table of result%rowtype;\n" +
-                "        c_t resultRowType;\n" +
-                "        begin\n" +
-                "        tempCursor:=" + function + ";\n" +
-                "        fetch tempCursor bulk collect into c_t;\n" +
-                "        close tempCursor;\n" +
-                "        forall indx in c_t.first..c_t.last\n" +
-                "        insert into result\n" +
-                "        values c_t(indx);\n" +
-                "        end;\n";
-        executeLine(databaseConnection, sqlCommand);
-
-        sqlCommand = "select * from result";
-
-
-        try (PreparedStatement resultPreparedStatement = connection.prepareStatement(sqlCommand)) {
+        try (PreparedStatement resultPreparedStatement = connection.prepareStatement(prepForResultSet(databaseConnection, function, columnsForTable))) {
             try (ResultSet resultSet = resultPreparedStatement.executeQuery()) {
                 result.append("\nContainers to load:\n\t");
                 while (resultSet.next()) {
@@ -332,8 +274,7 @@ public class DatabaseFunctions {
             databaseConnection.registerError(ex);
         }
 
-        sqlCommand = "drop table result";
-        executeLine(databaseConnection, sqlCommand);
+        executeLine(databaseConnection, DROP_TABLE_RESULT);
 
         return result.toString();
     }
