3bf44a498f6729c31a872f39bb17c96686c10a8a
1190772 <1190772@isep.ipp.pt>
2022-01-23 17:07:28 +0000

    LAP22G33-252 #close UC401 [Review]

--- src/main/java/lapr/project/ui/console/CriticalPortsUI.java
+++ src/main/java/lapr/project/ui/console/CriticalPortsUI.java
@@ -3,7 +3,10 @@ package lapr.project.ui.console;
 import lapr.project.controller.App;
 import lapr.project.controller.CriticalPortsController;
 import lapr.project.controller.ShortestPathController;
+import lapr.project.model.FreightNetworkVertex;
+import lapr.project.model.Port;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class CriticalPortsUI implements Runnable{
@@ -15,7 +18,16 @@ public class CriticalPortsUI implements Runnable{
 
     @Override
     public void run() {
-        System.out.println("Number of ports:");
-        System.out.println(controller.getCriticalPorts(App.getInstance().getCompany().getFreightNetwork(), new Scanner(System.in).nextInt()));
+        System.out.println("Insert the number of ports:");
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        System.out.println("Calculating...");
+        List<Port> ports = controller.getCriticalPorts(App.getInstance().getCompany().getFreightNetwork(), n);
+        System.out.println("Ports with greater centrality order:");
+        for (int i = 0; i < ports.size(); i++) {
+            System.out.printf("%d - %s\n", i+1, ports.get(i).getCountry().getName());
+        }
+        System.out.println("Press enter to continue...");
+        sc.nextLine();
     }
 }
